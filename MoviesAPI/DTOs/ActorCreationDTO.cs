00cdb39be9b8ae54a05fcd24603ca35f98a9245e
jacunar <josueally@gmail.com>
2023-12-19 05:06:49 -0600

    Validation file type added

--- MoviesAPI/DTOs/ActorCreationDTO.cs
+++ MoviesAPI/DTOs/ActorCreationDTO.cs
@@ -1,11 +1,12 @@
 ﻿using MoviesAPI.Validations;
 
-namespace MoviesAPI.DTOs; 
+namespace MoviesAPI.DTOs;
 public class ActorCreationDTO {
     [Required]
     [StringLength(120)]
     public string Name { get; set; } = string.Empty;
     public DateTime Birthdate { get; set; }
     [FileWeightValidation(4)]
+    [FileTypeValidation(FileTypeGroup.Picture)]
     public IFormFile Picture { get; set; } = null!;
 }
--- MoviesAPI/Validations/FileTypeValidation.cs
+++ MoviesAPI/Validations/FileTypeValidation.cs
@@ -1,8 +1,31 @@
 ﻿namespace MoviesAPI.Validations;
+
+public enum FileTypeGroup {
+    Picture
+}
+
 public class FileTypeValidation : ValidationAttribute {
+    private readonly string[] ValidTypes = new string[] { };
     public FileTypeValidation(string[] validTypes) {
         ValidTypes = validTypes;
     }
 
-    private readonly string[] ValidTypes { get; set; }
+    public FileTypeValidation(FileTypeGroup fileTypeGroup) {
+        if (fileTypeGroup == FileTypeGroup.Picture)
+            ValidTypes = new string[] { "image/jpeg", "image/png", "image/gif" };
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext) {
+        if (value == null)
+            return ValidationResult.Success;
+
+        IFormFile formFile = value as IFormFile;
+        if (formFile == null)
+            return ValidationResult.Success;
+
+        if (!ValidTypes.Contains(formFile.ContentType))
+            return new ValidationResult($"El tipo de archivo debe ser uno de los siguientes: {string.Join(", ", ValidTypes)}");
+
+        return ValidationResult.Success;
+    }
 }
